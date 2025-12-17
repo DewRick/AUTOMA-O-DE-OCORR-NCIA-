0d27497016426a72d693cb9e75f208699b5e30cb
DewRick <143047465+DewRick@users.noreply.github.com>
2025-12-17 13:43:06 -0300

    Create promotores.js

---
+++ promotores.js
@@ -0,0 +1,173 @@
+// promotores.js  (COMPLETO)
+
+const promotores = {
+  "Alan Nogueira": [
+    "Atacadão Resende",
+    "Dom Atacadista Resende",
+    "163-Havan-Resende",
+    "1197-lasa-Barra Mansa",
+    "230-Lasa-SHC Resende",
+    "606-Lasa-Patix Mix Resende Express",
+    "684-Lasa-Resende Express",
+    "Raia Resende B",
+    "Spani Atacadista Resente"
+  ],
+
+  "Alexandra": [
+    "Assai Barra da Tijuca",
+    "Caçula Barra Da Tijuca",
+    "100 - Lasa SHC Via Parque RJ",
+    "858 - Lasa SHC Uptown Express",
+    "Lilipe Festas",
+    "Aidan Barra",
+    "Upup Doces e Festas LTDA",
+    "Vivian Festas Barra da Tijuca"
+  ],
+
+  "Rickson": [
+    "Assaí - Conviva Barra",
+    "Dp Barra Da Tijuca 5",
+    "Drogaria Venancio - Av Embaixador Abelardo Bueno",
+    "Drogaria Venancio - Av Erico Verissimo",
+    "Drogaria Venancio Av Emb Abelardo Bueno 800",
+    "Drogaria Venancio Barra Da Tijuca 1",
+    "Drogaria Venancio Barra Da Tijuca 2",
+    "Drogasmil - Av Das Americas 3501",
+    "Drogasmil Barra Tijuca Cod. 29",
+    "1073 - Lasa - Shc Metropolitano",
+    "979 - Lasa - Shc Downtown Express",
+    "Prezunic Jardim Oceânico",
+    "1417 - Pa Barra",
+    "Raia Rj Barra Tijuca K"
+  ],
+
+  "Claudenice": [
+    "ATACADÃO JUIZ DE FORA",
+    "224 - DROGARIA ARAÚJO GETÚLIO VARGAS",
+    "1166 - LASA - SHC JARDIM NORTE",
+    "14 - LASA - JUIZ DE FORA",
+    "385 - LASA - RUI BARBOSA",
+    "MART MINAS JUIZ DE FORA",
+    "VILLEFORT JUIZ DE FORA"
+  ],
+
+  "Damião": [
+    "Venancio - Recreio Dos Bandeirantes",
+    "Drogasmil Barra Tijuca Cod. 45",
+    "1033 - Lasa - Shc Américas",
+    "5296 - Americanas Express - Avn Das Américas",
+    "534 - Lasa - Shc Rio Design Barra Express",
+    "Prezunic Barra",
+    "Prezunic Recreio",
+    "Pão De Açúcar Recreio Shopping",
+    "Raia Recreio Dos Bandeirantes - A"
+  ],
+
+  "Heyder": [
+    "Assaí Bangu",
+    "Atacadão Bangu",
+    "Caçula Bangu",
+    "Drogasmil Bangu",
+    "227 - Lasa - Shc Bangu",
+    "892 - Lasa - Bangu",
+    "Superdoces Bangu"
+  ],
+
+  "Letícia": [
+    "323 - Assaí Juiz De Fora",
+    "Carrefour Juiz De Fora",
+    "234 - Drogaria Araújo Auto Dos Passos Jf",
+    "Drogaria Pacheco Alto Dos Passos Jf",
+    "223 - Lasa - Shc Alameda",
+    "294 - Lasa - Shc Independencia",
+    "Mart Minas Teixeiras Juiz De Fora",
+    "Tete Festas São Sebastião Jf",
+    "Tete Festas Floriano Centro Jf"
+  ],
+
+  "Márcia": [
+    "338 - Santa Cruz Varandas",
+    "Assaí Cesario De Melo",
+    "90 - Lasa - Shc Park Campo Grande",
+    "Prezunic Campo Grande"
+  ],
+
+  "Maria": [
+    "Atacadão Barra Da Tijuca",
+    "Carrefour Barra Da Tijuca",
+    "Dp Shopping Barra Shopping",
+    "Drogaria Pacheco Barra Peninsula",
+    "Drogasmil Barra Tijuca Cod.66",
+    "Farmalife - Barrashopping",
+    "67 - Lasa - Shc Barrashop Rj",
+    "Prezunic Marapendi",
+    "Raia Barra Shopping",
+    "Raia Shopping New York"
+  ],
+
+  "Mellysa": [
+    "Caçula Campo Grande",
+    "Dom Atacadista Campo Grande",
+    "Drogaria Pacheco Campo Grande 4",
+    "Drogaria Pacheco Campo Grande 5",
+    "Drogaria Pacheco Campo Grande 6",
+    "Drogaria Pacheco Campo Grande 7",
+    "32 - Lasa - Campo Grande Rj",
+    "424 - Lasa - Barcelos Domingos",
+    "Superdoce Campo Grande",
+    "Ufa Campo Grande"
+  ],
+
+  "Nathani": [
+    "Assaí Freguesia",
+    "Drogaria Pacheco - Est. Do Pau Ferro 355",
+    "Drogaria Pacheco Freguesia 6",
+    "Drogaria Pacheco Pechincha 4",
+    "1451 - Lasa Park Jacarepagua",
+    "554 - Lasa - Freguesia Express",
+    "77 - Lasa - Shc Shop Rio",
+    "Prezunic Anil",
+    "Prezunic Freguesia",
+    "Prezunic Pechincha-Jacarepagua"
+  ],
+
+  "Patrícia": [
+    "Atacadão Realengo",
+    "Carrefour Sulacap",
+    "Dom Atacadista Realengo",
+    "Drogaria Pacheco - Realengo 2",
+    "Drogaria Pacheco Realengo 1",
+    "1062 - Lasa - Shc Parque Sulacap",
+    "Prezunic Realengo"
+  ],
+
+  "Paulo": [
+    "Atacadão Jacarepagua",
+    "Caçula - Plaza Taquara",
+    "Dom Atacadista Taquara",
+    "139 - Lasa - Taquara",
+    "1578 - Lasa - Shc Plaza Taquara",
+    "Prezunic Jauru",
+    "Prezunic Taquara",
+    "Ufa Taquara"
+  ],
+
+  "Priscila": [
+    "Assaí Santa Cruz",
+    "Atacadão Santa Cruz",
+    "Cia Do Doce Santa Cruz",
+    "369 - Lasa - Shc Patio Mix",
+    "471 - Lojas Americanas - R Dr Monteiro De Azevedo 34 - Itaguai - Rj",
+    "570 - Lasa - Shc Santa Cruz Express",
+    "Superdoces Santa Cruz"
+  ],
+
+  "William": [
+    "Assaí Campo Grande Rj",
+    "Atacadão Campo Grande",
+    "Carrefour Campo Grande",
+    "Drogaria Pacheco Campo Grande 9",
+    "Drogasmil Campo Grande Cod. 4",
+    "95 - Lasa - Shc Campo Grande"
+  ]
+};
