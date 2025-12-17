6becf27c237fca13454a27e510bddc85928a3d4c
DewRick <143047465+DewRick@users.noreply.github.com>
2025-12-17 13:41:03 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,50 @@
+ const produtos = {
+            sm: {
+                "80g": ["Dentadura 80g", "Beijos Morango 80g", "Banana 80g", "Minhoca Brilho 80g", "Minhoca Cítrica 80g", "Amora 80g", "Ursinho 80g", "Polvo 80g", "Sapo 80g", "Finiburguer 80g", "Aros 80g", "Ovo Frito 80g", "HP Coruja 80g", "HP Óculos 80g", "HP Chapeu 80g", "HP Raio 80g", "Rato 80g", "Escovinha 80g", "Oceanitos 80g", "Sorvetinho 80g", "Caveira Recheada 80g", "Fazendo Arte 80g", "Mix Surpresa 80g"],
+                "15g": ["Dentadura 15g", "Beijos Morango 15g", "Banana 15g", "Minhoca Brilho 15g", "Minhoca Cítrica 15g", "Amora 15g", "Ursinho 15g", "Dentadura da Bruxa 15g"],
+                "35g": ["Dentadura 35g", "Beijos Morango 35g", "Banana 35g", "Minhoca Brilho 35g", "Minhoca Cítrica 35g", "Amora 35g", "Ursinho 35g"],
+                "250g": ["Dentadura 250g", "Beijos 250g", "Banana 250g", "Minhoca Brilho 250g", "Minhoca Cítrica 250g", "Amora 250g", "Ursinho 250g", "Aros de Morango 250g", "Polvo 250g", "Sapo 250g", "Sorvetinho 250g", "Oceanitos 250g", "Mix Surpresa 250g", "Rato 250g", "Fazendo Arte 250g", "Caveira Recheada 250g", "Aranha 250g"],
+                "500g": ["Banana 500g", "Dentadura 500g", "Beijos Morango 500g", "Minhoca Brilho 500g", "Minhoca Cítrica 500g", "Amora 500g", "Ursinho 500g", "Aros de Morango 500g", "Polvo 500g", "Sapo 500g", "Sorvetinho 500g", "Oceanitos 500g", "Mix Surpresa 500g", "Rato 500g", "Fazendo Arte 500g", "Caveira Recheada 500g", "Aranha 500g"]
+            },
+            tubes: {
+                "80g": ["Morango Cítrico 80g", "Morango Nata 80g", "Tutti Fruti 80g", "Arco Íris 80g", "Twister Cítrico 80g", "Twister 80g", "Uva Cítrico 80g", "Uva 80g", "Tropical 80g", "Azedinho 80g", "Terror 80g"],
+                "35g": ["Morango 35g", "Morango Cítrico 35g", "Tutti Fruti 35g", "Twister 35g", "Azedíssimo 35g"],
+                "40g": ["Morango Cítrico 40g", "Morango Nata 40g", "Twister Cítrico 40g", "Twister 40g"],
+                "15g": ["Tubes Morango Cítrico 15g", "Tubes Morango Nata 15g", "Tubes Pinta Língua 15g", "Tubes Halloween 15g", "Três Cores Azedinho 15g", "Tutti Fruti 15g", "Twister 15g", "Morango com Laranja 15g", "Boo Uva 15g"],
+                "27g": ["Tubes Gigante 27g", "Tubes Iorgute 27g", "Tubes Morango 27g", "Tubes Morango Cítrico 27g", "Tubes Twister 27g", "Tubes Framboesa 27g", "Tubes Boo 27g"],
+                "240g": ["Morango 240g", "Morango Cítrico 240g"]
+            },
+            chicle: {
+                "80g": ["Chicle Salada de Fruta 80g", "Melancia 80g", "Ovo Dinossauro 80g"],
+                "140g": ["Fúria Ácida 140g"],
+                "230g": ["Melancia 230g", "Salada de Fruta 230g", "Ovo de Dino 230g"],
+                "250g": ["Chicle Melancia 250g", "Ovo Dinossauro 250g"],
+                "450g": ["Ovo de Dino 450g", "Melancia 450g", "Salada de Fruta 450g"]
+            },
+            mash: {
+                "80g": ["Torção Recheado 80g", "Torção Azul Rech 80g", "Torção Rosa Rech 80g", "Torção Tricolor 80g", "Torção Azul 80g", "Torção Rosa 80g", "Torção Branco 80g", "Alvorada 80g", "Flor 80g", "Coração 80g", "Borboleta 80g", "Morango 80g", "Vulcão Azul 80g", "Vulcão Rosa 80g", "Fondue 80g", "Maria Mole 80g", "Milho 80g", "Camping 80g", "Pipoca 80g", "Pistache 80g"],
+                "250g": ["Torção Recheado 250g", "Torção Azul Rech 250g", "Torção Rosa Rech 250g", "Torção Tricolor 250g", "Torção Azul 250g", "Torção Rosa 250g", "Torção Branco 250g", "Alvorada 250g", "Flor 250g", "Coração 250g", "Borboleta 250g", "Morango 250g", "Vulcão Azul 250g", "Vulcão Rosa 250g", "Fondue 250g", "Maria Mole 250g", "Milho 250g", "Camping 250g", "Pipoca 250g", "Mashboo 250g", "Mashboo RIP 250g", "Pistache 250g"],
+                "500g": ["Torção Recheado 500g", "Torção Azul Rech 500g", "Torção Rosa Rech 500g", "Torção Tricolor 500g", "Torção Azul 500g", "Torção Rosa 500g", "Torção Branco 500g", "Alvorada 500g", "Flor 500g", "Coração 500g", "Borboleta 500g", "Morango 500g", "Vulcão Azul 500g", "Vulcão Rosa 500g", "Fondue 500g", "Maria Mole 500g", "Milho 500g", "Camping 500g", "Pipoca 500g", "Pistache 500g"]
+            },
+            bemEstar: {
+                "18g": ["Vitamina C Banana Kids 18g", "Vitamina C Morango Kids 18g", "Vitamina C 18g", "Colágeno 18g", "Ursinho Diet 18g"]
+            },
+            zeroAcucares: {
+                "35g": ["Natureza Zero 35g", "Coração Zero 35g", "Fruts Zero 35g", "Tropical Zero 35g"]
+            },
+            algodaoPipo: {
+                "80g": ["Pipoca Banana 80g Pipo-Fini", "Pipoca Dentadura 80g Pipo-Fini", "Pipoca Beijos 80g Pipo-Fini"]
+            },
+            caldas: {
+                "1kg": ["Calda para Sorvete Dentadura 1kg", "Calda para Sorvete Beijos 1kg", "Calda para Sorvete Banana 1kg"]
+            },
+            caldaRecheio: {
+                "1.01kg": ["Recheio Banana 1.01kg", "Recheio Dentadura 1.01kg", "Recheio Beijos 1.01kg"]
+            },
+            drGood: {
+                "unidades": ["Dr. Good Vitaminas C 30 unidades", "Dr. Good Vitaminas C Kids 60 unidades", "Dr. Good Multigood Kids 30 unidades", "Dr. Good Multigood 30 unidades", "Dr. Good 5+Imune 30 unidades", "Dr. Good 5+Imune Diet 30 unidades", "Dr. Good Hair 60 unidades", "Dr. Good Melatonina 60 unidades"]
+            },
+            roller: {
+                "20g": ["Tiras Roller Frutas Azedinho 20g", "Tiras Roller Morango Azedinho 20g"]
+            }
+        };
